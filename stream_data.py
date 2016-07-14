8f6834e2aa908a27324071b4372399833b0b4974
ryanneely11 <ryanneely11@gmail.com>
2016-07-14 15:57:53 -0700

    Small changes relating to how datasets are organized for multiple channels

--- stream_data.py
+++ stream_data.py
@@ -17,19 +17,24 @@ meant to be implemented by a multiprocessing Process
 -array: a mp.Array memory-mapped array containing the data
 --array_dtype: the data type that you will be saving
 --lock: a threading.Lock object to control access to the Array and Event objects
---filename: the address to store the data (str)
+--fileroot: the address to create or open the data file (str)
+-channel: the channel number (will be used to name the dataset)
 """
 def file_stream(start_flag, data_flag, shared_array, 
-	dtype, filename):
+	dtype, fileroot, channel):
 	##open an hdf5 file
-	f_out = h5py.File(filename, 'w-')
+	f_out = h5py.File(fileroot, 'a')
 	##get the size of the mem-mapped array
 	array_len = len(array)
 	##create a dataset of size 0 that can be resized.
 	##setting chunk size to numPoints should speed up 
 	##operations (??)
-	dset = f_out.create_dataset("data", (0,), dtype = "float32", 
-								chunks = (array_len,), maxshape = (None,))
+	try:
+		dset = f_out.create_dataset(channel, (0,), dtype = "float32", 
+									chunks = (array_len,), maxshape = (None,))
+	except RuntimeError:
+		print "A dataset for " + channel + " already exists!"
+		break
 	##counter for the number of writes to file
 	written = 0
 	##check for start/stop flag
@@ -45,3 +50,14 @@ def file_stream(start_flag, data_flag, shared_array,
 			written+=1
 	f_out.close()
 	return 1
+
+
+"""
+The MIMO version of the above file. 
+"""
+def batch_stream(start_flag, fileroot, dtype, 
+	channel_names, data_flags, arrays):
+	pass
+
+
+
