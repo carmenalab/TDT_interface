474ea49602c5c9b2806af66830b7fece582d8aa5
ryanneely11 <ryanneely11@gmail.com>
2016-07-15 15:55:40 -0700

    adding a class of streaming objects. NEED to test to make sure this will pickle OK

--- stream_data.py
+++ stream_data.py
@@ -9,6 +9,50 @@ import h5py
 import multiprocessing as mp
 
 """
+a class that sets up a set of memory-mapped objects that can be shared
+across processes for streaming data to a file
+"""
+class StreamObj:
+	def __init__(self, size, dtype = None, tag = None):
+		##default to double
+		if self.dtype == None:
+			self.dtype = 'd'
+		else self.dtype = dtype
+		##the name of the dataset
+		if tag == None:
+			self.tag == "data"
+		else self.tag = tag
+		##the size of the data pipe array
+		self.size = size
+		##memory-mapped array object with automatic locking
+		self.array = mp.Array(dtype, size, lock = True)
+		##memory-mapped Event object to flag new data in the "pipe"
+		self.flag = mp.Event()
+
+	##a function to update new data into the pipe
+	def write_pipe(self,data):
+		if len(data) != self.size:
+			raise AssertionError("input data wrong size")
+		if self.flag.is_set():
+			raise AssertionError("unread data in pipe!")
+		##add new data to the array
+		self.array[:] = data
+		##signal new data is ready to be read
+		self.flag.set()
+
+	##a function to check if NEW(!) unread data is available and
+	##if so, return it
+	def read_pipe(self):
+		result = None
+		if self.flag.is_set():
+			result = array[:]
+			##cancel the flag until next write
+			self.flag.clear()
+		return result
+
+
+
+"""
 a function to save data from a memory-mapped array.
 meant to be implemented by a multiprocessing Process
 
@@ -18,33 +62,31 @@ meant to be implemented by a multiprocessing Process
 --array_dtype: the data type that you will be saving
 --filename: the address to create or open the data file (str)
 """
-def file_stream(start_flag, data_flag, shared_array, 
-	array_dtype, filename):
+def file_stream(start_flag, filename, stream_obj):
 	##open an hdf5 file
 	f_out = h5py.File(filename, 'a')
 	##get the size of the mem-mapped array
-	array_len = len(array)
+	array_len = len(stream_obj.array)
 	##create a dataset of size 0 that can be resized.
 	##setting chunk size to numPoints should speed up 
 	##operations (??)
 	try:
-		dset = f_out.create_dataset("data", (0,), dtype = "array_dtype", 
+		dset = f_out.create_dataset(stream_obj.tag, (0,), dtype = stream_obj.dtype, 
 									chunks = (array_len,), maxshape = (None,))
 	except RuntimeError: 
-		print "A dataset for " + channel + " already exists!"
+		print "A dataset for " + stream_obj.tag + " already exists!"
 		break
 	##counter for the number of writes to file
 	written = 0
 	##check for start/stop flag
-	while run_flag.is_set():
+	while start_flag.is_set():
 		##check for new data in the pipe
-		if data_flag.is_set():
+		data_read = stream_obj.read_pipe()
+		if data_read != None:
 			##increase the size of the dataset to accomodate new data block
 			dset.resize((dset.size+array_len,))
 			##append new data to the end of the current dset
-			dset[(written*array_len):] = array[:]
-			##signal that data has been written
-			new_flag.clear()
+			dset[(written*array_len):] = data_read
 			written+=1
 	f_out.close()
 	return 1
@@ -54,21 +96,16 @@ def file_stream(start_flag, data_flag, shared_array,
 The MIMO version of the above file. 
 
 -start_flag is a mem-mapped flag object
--data_flags: list of mp flags for each array object
--array_dtype: data type to save data as 
--filenames: list of filenames to save data as
+-stream_objs: a list of StreamObj objects
 """
-def batch_stream(start_flag, data_flags, arrays, array_dtype, filenames):
+def batch_stream(start_flag, stream_objs):
 	##how many channels are we working with, 
 	##and make sure numbers match
-	num_chans = len(arrays)
-	if num_chans != len(filenames) or num_chans != len(data_flags):
-		raise ValueError("Mismatched channel names/flags/numbers")
+	num_chans = len(stream_objs)
 	##create a worker pool to handle the data saving
 	pool = mp.Pool(num_chans)
 	for i in range(num_chans):
-		pool.apply_async(file_stream, args = (start_flag, data_flags[i], arrays[i],
-			array_dtype, filenames[i]))
+		pool.apply_async(file_stream, args = (start_flag, stream_objs[i]))
 	
 
 
