75b40a8dfc49915fd7608077b2400011eb25c1f2
ryanneely11 <ryanneely11@gmail.com>
2016-07-18 15:57:09 -0700

    Trying and failin o get around pickling issues

--- stream_data.py
+++ stream_data.py
@@ -9,7 +9,8 @@ import h5py
 import multiprocessing as mp
 
 
-##DUMB: class objects will not pickle! Need to re-do this whole thing.
+##TODO something isn't working right; may need to try mp.Manager
+##to send the class object. Sigh so complicated!
 
 """
 a class that sets up a set of memory-mapped objects that can be shared
@@ -18,13 +19,15 @@ across processes for streaming data to a file
 class StreamObj:
 	def __init__(self, size, dtype = None, tag = None):
 		##default to double
-		if self.dtype == None:
+		if dtype == None:
 			self.dtype = 'd'
-		else self.dtype = dtype
+		else:
+			self.dtype = dtype
 		##the name of the dataset
 		if tag == None:
 			self.tag == "data"
-		else self.tag = tag
+		else:
+			self.tag = tag
 		##the size of the data pipe array
 		self.size = size
 		##memory-mapped array object with automatic locking
@@ -32,26 +35,28 @@ class StreamObj:
 		##memory-mapped Event object to flag new data in the "pipe"
 		self.flag = mp.Event()
 
-	##a function to update new data into the pipe
-	def write_pipe(self,data):
-		if len(data) != self.size:
-			raise AssertionError("input data wrong size")
-		if self.flag.is_set():
-			raise AssertionError("unread data in pipe!")
-		##add new data to the array
-		self.array[:] = data
-		##signal new data is ready to be read
-		self.flag.set()
+##a function to update new data into a "pipe"
+##stream_obj is a stream object class
+def write_pipe(stream_obj,data):
+	if len(data) != stream_obj.size:
+		raise AssertionError("input data wrong size")
+	if stream_obj.flag.is_set():
+		raise AssertionError("unread data in pipe!")
+	##add new data to the array
+	stream_obj.array[:] = data
+	##signal new data is ready to be read
+	stream_obj.flag.set()
 
-	##a function to check if NEW(!) unread data is available and
-	##if so, return it
-	def read_pipe(self):
-		result = None
-		if self.flag.is_set():
-			result = array[:]
-			##cancel the flag until next write
-			self.flag.clear()
-		return result
+##a function to check if NEW(!) unread data is available and
+##if so, return it
+##stream_obj is a StreamObj class object (above)
+def read_pipe(stream_obj):
+	result = None
+	if stream_obj.flag.is_set():
+		result = stream_obj.array[:]
+		##cancel the flag until next write
+		stream_obj.flag.clear()
+	return result
 
 
 
@@ -69,7 +74,7 @@ def file_stream(start_flag, filename, stream_obj):
 	##open an hdf5 file
 	f_out = h5py.File(filename, 'a')
 	##get the size of the mem-mapped array
-	array_len = len(stream_obj.array)
+	array_len = stream_obj.size
 	##create a dataset of size 0 that can be resized.
 	##setting chunk size to numPoints should speed up 
 	##operations (??)
@@ -78,13 +83,13 @@ def file_stream(start_flag, filename, stream_obj):
 									chunks = (array_len,), maxshape = (None,))
 	except RuntimeError: 
 		print "A dataset for " + stream_obj.tag + " already exists!"
-		break
+	
 	##counter for the number of writes to file
 	written = 0
 	##check for start/stop flag
 	while start_flag.is_set():
 		##check for new data in the pipe
-		data_read = stream_obj.read_pipe()
+		data_read = read_pipe(strem_obj)
 		if data_read != None:
 			##increase the size of the dataset to accomodate new data block
 			dset.resize((dset.size+array_len,))
